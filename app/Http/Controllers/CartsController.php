04318fb549f92cf32334e07f9c70e39795d1824c
ujez <ujezujez@gmail.com>
2022-06-02 12:07:56 +0100

    added the checkout component

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -15,7 +15,7 @@ class CartsController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.checkout');
     }
 
     /**
@@ -36,49 +36,42 @@ class CartsController extends Controller
      */
     public function store(Request $request)
     {
-        if(!$request->get('product_id')){
+        if (!$request->get('product_id')) {
             return [
-                'message'=>'Cart items returned',
-                'items' => Cart::where('user_id', auth()->user()->id)->sum('quantity'), 
+                'message' => 'Cart items returned',
+                'items' => Cart::where('user_id', auth()->user()->id)->sum('quantity'),
             ];
         }
-
 
         // Getting product details.
 
         $product = Product::where('id', $request->get('product_id'))->first();
 
-        $productFoundInCart = Cart::where('product_id', 
-        $request->get('product_id'))->pluck('id');
+        $productFoundInCart = Cart::where('product_id',
+            $request->get('product_id'))->pluck('id');
 
-        
-
-        if($productFoundInCart->isEmpty())
-        {
+        if ($productFoundInCart->isEmpty()) {
             // Adding Product in cart.
 
-            $cart  = Cart::create([
+            $cart = Cart::create([
                 'product_id' => $product->id,
                 'quantity' => 1,
                 'price' => $product->sale_price,
                 'user_id' => auth()->user()->id,
             ]);
-        }
-        else
-        {
+        } else {
             // Incrementing Product Quantity.
 
             $cart = Cart::where('product_id', $request->get('product_id'))
-            ->increment('quantity');
+                ->increment('quantity');
         }
 
-         // Check user cart items.
+        // Check user cart items.
 
-        if($cart)
-        {
+        if ($cart) {
             return [
-                'message'=>'Cart Updated',
-                'items' => Cart::where('user_id', auth()->user()->id)->sum('quantity'), 
+                'message' => 'Cart Updated',
+                'items' => Cart::where('user_id', auth()->user()->id)->sum('quantity'),
             ];
         }
     }
@@ -127,4 +120,35 @@ class CartsController extends Controller
     {
         //
     }
+
+    public function getCartItemsForCheckout()
+    {
+
+        $cartItems = Cart::with('product')->where('user_id', auth()->user()->id)->get();
+
+        $finalData = [];
+
+        $amount = 0;
+
+        if (isset($cartItems)) {
+            foreach ($cartItems as $cartItem) {
+                if ($cartItem->product) {
+                    foreach ($cartItem->product as $cartProduct) {
+                        if ($cartProduct->id == $cartItem->product_id) {
+                            $finalData[$cartItem->product_id]['id'] = $cartProduct->id;
+                            $finalData[$cartItem->product_id]['name'] = $cartProduct->name;
+                            $finalData[$cartItem->product_id]['quantity'] = $cartItem->quantity;
+                            $finalData[$cartItem->product_id]['sale_price'] = $cartItem->price;
+                            $finalData[$cartItem->product_id]['total'] = $cartItem->price * $cartItem->quantity;
+                            $amount += $cartItem->price * $cartItem->quantity;
+                            $finalData['totalAmount'] = $amount;
+                        }
+                    }
+                }
+            }
+        }
+
+        return response()->json($finalData);
+    }
+
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -26,6 +26,7 @@ Vue.use(Vuetoastr);
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('add-to-cart-button', require('./components/AddToCart.vue').default);
 Vue.component('cart', require('./components/Cart.vue').default);
+Vue.component('checkout', require('./components/Checkout.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
